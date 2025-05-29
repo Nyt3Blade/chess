b83575d627c6efe02f4ae6cf5096f57cf205eab7
Yash <yashyadav942004@gmail.com>
2025-05-29 20:15:15 +0530

    setting up a simple backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,12 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
+const GameManager_1 = require("./GameManager");
 const PORT = process.env.PORT || 8080;
 const wss = new ws_1.WebSocketServer({ port: Number(PORT) });
+const gameManager = new GameManager_1.GameManager();
 wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
-    ws.on('message', function message(data) {
-        console.log('Received: %s', data.toString());
-    });
-    ws.send('something');
+    gameManager.addUser(ws);
+    ws.on("disconnect", () => gameManager.removeUser(ws));
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,13 @@
 import { WebSocketServer } from 'ws';
+import { GameManager } from './GameManager';
 
 const PORT = process.env.PORT || 8080;
 
 const wss = new WebSocketServer({ port: Number(PORT) });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+const gameManager = new GameManager();
 
-  ws.on('message', function message(data) {
-    console.log('Received: %s', data.toString());
-  });
-  
-  ws.send('something');
+wss.on('connection', function connection(ws) {
+    gameManager.addUser(ws);
+    ws.on("disconnect", () => gameManager.removeUser(ws));
 });
